2a4199721d7648673cb560326f0b30c73548283c
Adam Braimbridge <adam@braimbridge.com>
2019-03-31 21:06:11 +0100

    WIP - Adding verification handler

--- src/verify.js
+++ src/verify.js
@@ -1,7 +1,11 @@
 const verify = require("../lib/verify")
 
-module.exports = (request, response) => {
-    verify(request)
-
-    response.status(200).send('Hello World!');
+module.exports = async (request, response) => {
+    try {
+        await verify(request)
+        response.status(200).send('Hello World!')
+    } 
+    catch (error) {
+        response.status(500).send(error)
+    }
 }
