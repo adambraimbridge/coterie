cc3ecc9ab7017e6b7ed8d85aca181e1ddebc9cc2
Adam Braimbridge <adam@braimbridge.com>
2019-03-31 21:57:44 +0100

    WIP - Adding verification handler

--- src/verify.js
+++ src/verify.js
@@ -1,6 +1,10 @@
 const verify = require("../lib/verify")
 
 module.exports = (request, response) => {
+
+    console.error("Slack token:",process.env.SLACK_TOKEN)
+    console.error("Request token:",request.body.token)
+    
     verify(request, response)
     response.status(200).send(request.body.challenge)
 }
