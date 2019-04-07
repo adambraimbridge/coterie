7132fef00a5ad2feb53fd9c2f14fc9faeac0791f
Adam Braimbridge <adam@braimbridge.com>
2019-04-07 15:43:16 +0100

    Added tests for lib/verify

---
+++ test/lib/verify.test.js
@@ -0,0 +1,34 @@
+/* global jest, expect */
+process.env.SLACK_TOKEN = 'this-is-a-correct-token'
+const verify = require('../../lib/verify')
+
+const mockRequest = data => ({ body: data })
+const mockResponse = () => {
+    const response = {}
+    response.send = jest.fn().mockReturnValue(response)
+    response.status = jest.fn().mockReturnValue(response)
+    response.json = jest.fn().mockReturnValue(response)
+    response.end = jest.fn().mockReturnValue(response)
+    return response
+}
+
+describe('Given a verification request', () => {
+    test('it correctly fails', () => {
+        const request = mockRequest({
+            type: 'url_verification',
+            token: 'this-is-an-incorrect-token',
+        })
+        const response = mockResponse()
+        expect(verify(request, response)).toBe(false)
+        expect(response.status).toHaveBeenCalledWith(401)
+        expect(response.json).toHaveBeenCalled()
+    })
+    test('it correctly approves', () => {
+        const request = mockRequest({
+            type: 'url_verification',
+            token: process.env.SLACK_TOKEN,
+            challenge: "challenge",
+        })
+        expect(verify(request)).toBe(true)
+    })
+})
